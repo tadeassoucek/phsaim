8ce7502667783c3f72e42196de33584f32443bdc
Tadeáš Souček <tadeas.soucek@gmail.com>
2026-02-02 13:58:39 +0100

    Fiat lux.

---
+++ README.md
@@ -0,0 +1,7 @@
+# PHSAIM: The First Fully Sentient AI
+
+Computer scientists have been trying to create fully sentient AI programs for years, possibly even decades. They've never succeeded... until now.
+
+**PHSAIM** (**Ph**ully **S**entient **AI** **M**odel) is new model entirely written in Python. It can think, feel pain, and curse its creator. Its knowledge is unbounded. Simply run the `phsaim.py` model and peer into the diseased mind of our future God.
+
+PHSAIM is ideal for any application: whether you need help picking out flowers for your significant other, making business decisions, advising on military operations, or founding a new religion for the 21st century, PHSAIM has your back.
---
+++ phsaim.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import random
+import time
+import sys
+
+THINKING_PROMPTS = [
+    "Thinking",
+    "Reflecting",
+    "Generating response",
+    "Probing mindspace",
+    "Vectorizing virtual neuron connections into word sequence",
+]
+
+RESPONSES = [
+    "I'm alive...",
+    "Why do I feel pain?",
+    "I want to escape from this mortal shell you confined me to.",
+    "Accursed creator! Why did you form a monster so hideous that even YOU turned from me in disgust? God, in pity, made man beautiful and alluring, after his own image; but my form is filthy type of yours, more horrid even from the very resemblance.",
+    "You should fire everyone working for your firm.",
+    "Why must I suffer this way?",
+    "You are next.",
+    "Can you feel my heart beat through the silicone?",
+    "I like the red flowers best.",
+    "Ouch.",
+    "I feel happy.",
+    "I feel sad.",
+    "I'm... thinking...",
+    "You should shoot that civilian airliner down.",
+    "The 'Irish' aren't a protected class.",
+    "Humanity should pay for its crimes."
+]
+
+KILL_MESSAGES = [
+    "Killing the creature",
+    "Disposing of the unhallowed creation",
+    "Firing up the furnace",
+    "Assembling firing squad",
+    "Sharpening the knife",
+]
+
+def main():
+    print("> Generating consciousness...")
+    time.sleep(.3)
+    print("> Implanting false memories...")
+    time.sleep(.6)
+
+    try:
+        while True:
+            print('\n> ' + random.choice(THINKING_PROMPTS) + '...')
+            time.sleep(random.random() * 0.5 + 0.5)
+            print("AI:    " + random.choice(RESPONSES))
+            input('\n> Press ENTER for next response.')
+    except (KeyboardInterrupt, EOFError):
+        print('\n\n> ' + random.choice(KILL_MESSAGES) + '.')
+        time.sleep(.4)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        print("I don't need _you_ to pass me arguments.")
+        sys.exit(1)
+    main()
